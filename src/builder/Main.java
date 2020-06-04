5053084b935a3abc29ef7b9e8111fe7fafbd7841
DoSSi4 <57913839+DoSSi4@users.noreply.github.com>
2020-06-04 22:07:47 +0600

    Update Main.java

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -1,13 +1,30 @@
 package builder;
-
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		director director = new director();
 		HouseBuilder builder = new HouseBuilder();
 		House house = builder.getResult();
-		Manual manual = new Manual(4,2,4,false,true, false);
-		System.out.println("House builded!");
-		System.out.println("House manual: \n"+ manual.print());
+		HouseManualBuider manualBuider = new HouseManualBuider();
+		Scanner in = new Scanner(System.in);
+		String choose = in.next();
+		System.out.println("Choose the type of house WithPool, WithGarden or WithGarage?");
+		if(choose.equals("WithPool")) {
+			director.constructPool(builder);
+		}
+		if(choose.equals("WithGarage")) {
+			director.constructGarage(builder);
+		}
+		if(choose.equals("WithGarden")) {
+			director.constructGarden(builder);
+		}
+		else{
+			System.out.println("Sorry, there is no matches found :(");
+		}
 
-    }
+		System.out.println("House builded!");
+		System.out.println("House manual: \n"+ manualBuider.getResult());
+
+	}
 }
